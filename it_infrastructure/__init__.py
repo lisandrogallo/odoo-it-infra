ace657dbc06e2accc7fd303aaf40ccb5855080d0
Liso Gallo <liso@riseup.net>
2014-12-24 10:41:18 -0300

    [DEL] Some models delete

--- it_infrastructure/__init__.py
+++ it_infrastructure/__init__.py
@@ -5,13 +5,11 @@ import database
 import database_type
 import database_backup
 import db_backup_policy
-# import db_filter
 import environment
 import environment_repository
 import environment_version
 import instance
 import instance_host
-import mailserver
 import partner
 import repository
 import repository_branch
@@ -22,8 +20,6 @@ import server_configuration_command
 import server_hostname
 import server_repository
 import server_service
-# import service
-# import service_command
 import wizard
 
 import supply
