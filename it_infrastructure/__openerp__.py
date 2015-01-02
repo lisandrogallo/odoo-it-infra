2106a18f366c8f7383d1c43c025745d73eb3e5bb
Liso Gallo <liso@riseup.net>
2015-01-02 13:04:31 -0300

    [ADD] Device model views

--- it_infrastructure/__openerp__.py
+++ it_infrastructure/__openerp__.py
@@ -26,7 +26,6 @@
         u'view/instance_view.xml',
         u'view/supply_view.xml',
         u'view/device_view.xml',
-        u'view/equipment_view.xml',
         u'view/repository_branch_view.xml',
         u'view/server_configuration_view.xml',
         u'view/server_change_view.xml',
