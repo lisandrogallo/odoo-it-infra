b652c9310d1912531d4389fe02bf137817529087
Liso Gallo <liso@riseup.net>
2015-01-22 09:30:10 -0300

    [FIX] Several changes and adjustments

--- it_infrastructure/__init__.py
+++ it_infrastructure/__init__.py
@@ -1,33 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import command
-# import database
-# import database_type
-# import database_backup
-# import database_backup_policy
-# import environment
-# import environment_repository
-# import environment_version
-# import instance
-# import instance_host
-# import partner
-import repository
-import repository_branch
-import wizard
-
 import category
-import supply
-import supply_category
+import command
 import equipment
+import computer
+import computer_change
 import device
 import device_category
-import computer
-import software
+import repository
+import repository_branch
 import server
-import software_category
-import computer_change
 import server_configuration
 import server_configuration_command
 import server_hostname
 import server_repository
+import software
+import software_category
+import supply
+import supply_category
 import workstation
--- it_infrastructure/wizard/__init__.py
+++ it_infrastructure/wizard/__init__.py
@@ -1,5 +1,1 @@
 # -*- coding: utf-8 -*-
-
-import duplicate_database_wizard
-import restore_database_wizard
-import change_database_passwd_wizard
