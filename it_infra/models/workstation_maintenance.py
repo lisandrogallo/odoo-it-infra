6a59f0cdf9250f18b06342e3d7c7b6aae08bc306
Liso Gallo <liso@riseup.net>
2018-02-27 10:32:37 -0300

    [IMP] Required fields and default order on workstation maintenance

--- it_infra/models/workstation_maintenance.py
+++ it_infra/models/workstation_maintenance.py
@@ -7,13 +7,16 @@ from datetime import datetime
 class WorkstationMaintenance(models.Model):
 
     _name = 'it_infra.workstation_maintenance'
+    _order = "date desc"
 
     name = fields.Char(
-        string='Description'
+        string='Description',
+        required=True
     )
 
     date = fields.Date(
-        default=datetime.today()
+        default=datetime.today(),
+        required=True
     )
 
     workstation_id = fields.Many2one(
